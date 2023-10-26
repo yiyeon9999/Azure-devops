3203c6b29017aa0a3deee519cfb5483f45e8f9c1
yiyeon9999 <yiyeon9999@gmail.com>
2023-10-26 22:23:38 +0700

    change file mcommands

--- commands.sh
+++ commands.sh
@@ -1,1 +1,1 @@
-az webapp up --name flaskappduypq5 --resource-group Azuredevops --sku B1 --logs --runtime PYTHON:3.9
+az webapp up --name flaskappyendt --resource-group Azuredevops --sku B1 --logs --runtime PYTHON:3.9
