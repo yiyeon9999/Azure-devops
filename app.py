3ad724e786adc3f1f436bc1700eaad036099c700
yiyeon9999 <yiyeon9999@gmail.com>
2023-10-26 18:13:37 +0700

    change app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
 
-#import sklearn
 import pandas as pd
+# from sklearn.externals import joblib
 import joblib
 from sklearn.preprocessing import StandardScaler
 
@@ -28,11 +28,12 @@ def home():
 @app.route("/predict", methods=['POST'])
 def predict():
     # Performs an sklearn prediction
+
     try:
         # Load pretrained model as clf. Try any one model. 
-        clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
+        # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
         # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
-        # clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
+        clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
     except:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
@@ -46,4 +47,4 @@ def predict():
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
